c4c42d7ec7d75057f810f83728ba2a7816696a17
Ogo Ozotta <88560609+ogochukwu-ozotta@users.noreply.github.com>
2024-01-18 23:01:30 +0000

    scrapped the guardian for climate news

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ npm init
 npm i cheerio
 npm i express
 npm i axios
+npm i nodemon
+npm run start 
--- index.js
+++ index.js
@@ -5,4 +5,27 @@ const cheerio = require('cheerio')
 
 const app = express()
 
-app.use 
+const articles = []
+
+app.get('/', (req,res) => {
+    res.json('Welcome to my Climate Change News API')
+})
+
+app.get('/news', (req,res) => {
+    axios.get('https://www.theguardian.com/environment/climate-crisis')
+    .then((response) => {
+        const html = response.data
+        const $ = cheerio.load(html)
+
+        $('a:contains("climate")', html).each(function () {
+            const title = $(this).text()
+            const url = $(this).attr('href')
+            articles.push({
+                title,
+                url
+            })
+        })
+        res.json(articles)
+    }).catch((err) => console.log(err))
+})
+app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
